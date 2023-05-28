eb5a972ebb49a8cbfd1db576c895009057884655
osparking <jbpark03@gmail.com>
2023-05-28 21:23:53 +0900

    Users domain class

--- src/main/java/space/bumtiger/db/model/Users.java
+++ src/main/java/space/bumtiger/db/model/Users.java
@@ -1,5 +1,14 @@
 package space.bumtiger.db.model;
 
-public class Users {
+import org.springframework.data.annotation.Id;
 
+public record Users(@Id Integer id, 
+		String lastname, String firstname) {
+	public String fullname() {
+		StringBuffer buffer = new StringBuffer(lastname);
+		buffer.append(" ");
+		buffer.append(firstname);
+		return buffer.toString();
+	}
+	
 }
