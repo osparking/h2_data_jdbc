53d92eae206861416549dc7f0a9e902f1289750e
osparking <jbpark03@gmail.com>
2023-05-28 21:29:42 +0900

    user service class with 6 methods

--- src/main/java/space/bumtiger/db/service/UserService.java
+++ src/main/java/space/bumtiger/db/service/UserService.java
@@ -1,5 +1,40 @@
 package space.bumtiger.db.service;
 
-public class UserService {
+import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import space.bumtiger.db.model.Users;
+import space.bumtiger.db.repository.UserRepository;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	
+	public List<Users> getUsers() {
+		return userRepository.findAll();
+	}
+	
+	public Users getUserById(Integer id) {
+		return userRepository.findById(id).orElse(null);
+	}
+	
+	public Users createUser(Users user) {
+		return userRepository.save(user);
+	}
+	
+	public Users updateUser(Users user) {
+		return userRepository.save(user);
+	}
+	
+	public Users patchUser(Users user) {
+		return userRepository.save(user);
+	}
+	
+	public void deleteUser(Users user) {
+		userRepository.delete(user);
+	}
+	
 }
