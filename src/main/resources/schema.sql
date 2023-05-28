f1af4a9e5bd3f71031b76fc46609f56819e09d2b
osparking <jbpark03@gmail.com>
2023-05-28 19:28:02 +0900

    insert 3 users into users table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,3 +2,7 @@ create table users (
 	id int auto_increment,
 	lastname varchar(255) not null,
 	firstname varchar(255) not null);
+	
+insert into users(lastname, firstname) values ('김', '민수');	
+insert into users(lastname, firstname) values ('황', '신혜');	
+insert into users(lastname, firstname) values ('박', '홍길');	
